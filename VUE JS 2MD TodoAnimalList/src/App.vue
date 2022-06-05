689d423e3de30117c8292e6fc8b7a7fcdf9355c1
Nikita <kipron11@gmail.com>
2022-06-05 03:02:49 +0300

    Animal List Done 99%%%

---
+++ VUE JS 2MD TodoAnimalList/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="App">
+    <HomeView />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HomeView from "./views/HomeView.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    HomeView,
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+.App {
+  display: flex;
+  justify-content: center;
+  margin-top: 15%;
+}
+</style>
